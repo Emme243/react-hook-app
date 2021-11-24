6b5811c8e5e651ece2f699b96d149b46699d8115
Emmanuel <emmlop243@gmail.com>
2021-11-24 13:42:42 -0600

    Ejercicio con useCallback y React.memo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import MemoHook from "./components/06-memos/MemoHook";
+import { Padre } from "./components/07-tarea-memo/Padre";
+// import CallbackHook from "./components/06-memos/CallbackHook";
+// import MemoHook from "./components/06-memos/MemoHook";
 // import Memorize from "./components/06-memos/Memorize";
 // import Layout from "./components/05-useLayoutEffect/Layout";
 // import RealExample from "./components/04-useRef/RealExample";
@@ -12,4 +14,4 @@ import MemoHook from "./components/06-memos/MemoHook";
 // import CounterApp from "./components/01-useState/CounterApp";
 // import HookApp from "./HookApp";
 
-ReactDOM.render(<MemoHook />, document.getElementById("root"));
+ReactDOM.render(<Padre />, document.getElementById("root"));
